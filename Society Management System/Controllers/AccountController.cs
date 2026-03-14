5cc7b8aae39e301966b3d53007b3f8c2c880a733
Trushali Sorathiya <sorathiyatrushali225@gmail.com>
2026-03-14 14:42:10 +0530

    Account

--- Society Management System/Controllers/AccountController.cs
+++ Society Management System/Controllers/AccountController.cs
@@ -1,34 +1,76 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace Society_Management_System.Controllers
 {
     public class AccountController : Controller
     {
-        // Login Page
+
+        // LOGIN PAGE
         public IActionResult Login()
         {
             return View();
         }
 
-        // Forgot Password Page (GET)
+        // FORGOT PASSWORD PAGE
         public IActionResult Forget()
         {
             return View();
         }
 
-        // Forgot Password Form Submit (POST)
+        // SEND OTP
         [HttpPost]
-        public IActionResult Forget(string Email)
+        public IActionResult SendOTP(string email)
         {
-            if (string.IsNullOrEmpty(Email))
+            if (string.IsNullOrEmpty(email))
             {
-                ViewBag.Error = "Please enter your email.";
-                return View();
+                ViewBag.Error = "Please enter email";
+                return View("Forget");
             }
 
-            // Example logic (you can connect to database here)
-            ViewBag.Message = "Password reset link sent to your email.";
+            Random rand = new Random();
+            int otp = rand.Next(100000, 999999);
 
+            HttpContext.Session.SetString("OTP", otp.ToString());
+            HttpContext.Session.SetString("Email", email);
+
+            ViewBag.Message = "OTP Sent Successfully";
+
+            return RedirectToAction("VerifyOTP");
+        }
+
+        // VERIFY OTP PAGE
+        public IActionResult VerifyOTP()
+        {
+            return View();
+        }
+
+        // VERIFY OTP POST
+        [HttpPost]
+        public IActionResult VerifyOTP(string otp)
+        {
+            string sessionOTP = HttpContext.Session.GetString("OTP");
+
+            if (otp == sessionOTP)
+            {
+                return RedirectToAction("ResetPassword");
+            }
+
+            ViewBag.Error = "Invalid OTP";
+            return View();
+        }
+
+        // RESET PASSWORD PAGE
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        // RESET PASSWORD POST
+        [HttpPost]
+        public IActionResult ResetPassword(string password)
+        {
+            ViewBag.Message = "Password Reset Successfully";
             return View();
         }
     }
