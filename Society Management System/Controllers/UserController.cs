e3777067d6da502c5e4ff1debda1946ac88d2fd0
Trushali Sorathiya <sorathiyatrushali225@gmail.com>
2026-03-15 21:57:49 +0530

    User

---
+++ Society Management System/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Society_Management_System.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Complaint()
+        {
+            return View();
+        }
+    }
+}
