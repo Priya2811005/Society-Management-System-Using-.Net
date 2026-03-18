a983ff4860c59167552ebb05d9daa92b63db88d3
tejalrupapara <tejalrupapara0@gmail.com>
2026-03-18 11:01:02 +0530

    Changes

--- Society Management System/Controllers/UserController.cs
+++ Society Management System/Controllers/UserController.cs
@@ -4,15 +4,28 @@ namespace Society_Management_System.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Complaint()
+        
+        public IActionResult Index()
         {
-            return View();
+            // Sample data (later you can fetch from DB)
+            var users = new List<UserModel>()
+            {
+                new UserModel { Name="Anuj Patel", FlatNo="101", Wing="A" },
+                new UserModel { Name="Maya Desai", FlatNo="104", Wing="G" },
+                new UserModel { Name="Priya Desai", FlatNo="701", Wing="H" },
+                new UserModel { Name="Dev Bhut", FlatNo="201", Wing="B" },
+                new UserModel { Name="Shruti Shah", FlatNo="402", Wing="C" },
+                new UserModel { Name="Nisha Verma", FlatNo="503", Wing="E" }
+            };
+
+            return View(users);
         }
-
-
-        public IActionResult addvisitor()
+        // Simple Model
+        public class UserModel
         {
-            return View();
+            public string Name { get; set; }
+            public string FlatNo { get; set; }
+            public string Wing { get; set; }
         }
     }
 }
