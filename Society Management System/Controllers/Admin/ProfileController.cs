c95546cace7092486cb5b324e32efe73bdf186b5
Priya Limbasiya <your_email@example.com>
2026-03-24 08:12:28 +0530

    Namespace

---
+++ Society Management System/Controllers/Admin/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Society_Management_System.Controllers.Admin
+{
+    [Route("Admin/[controller]/[action]")]
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Admin/Profile/Index.cshtml");
+        }
+    }
+}
