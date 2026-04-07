90eb710da680cc8bfa1c8a1a824bea77f5cad625
Trushali Sorathiya <sorathiyatrushali225@gmail.com>
2026-04-07 09:43:08 +0530

    User

---
+++ Society Management System/Models/User/Visitor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Society_Management_System.Models
+{
+    public class Visitor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public DateTime VisitDate { get; set; }
+        public string Purpose { get; set; }
+    }
+}
