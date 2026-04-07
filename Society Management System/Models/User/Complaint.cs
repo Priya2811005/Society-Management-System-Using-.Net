f2ed35fedf4ce9e8b966cbee628ddad8dab46f5c
Trushali Sorathiya <sorathiyatrushali225@gmail.com>
2026-04-07 09:42:26 +0530

    User

---
+++ Society Management System/Models/User/Complaint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Society_Management_System.Models
+{
+    public class Complaint
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
