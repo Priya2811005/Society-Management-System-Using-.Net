d5e8503ea8f1ac1f155249b1849f41524d2852fd
Trushali Sorathiya <sorathiyatrushali225@gmail.com>
2026-04-07 09:42:52 +0530

    User

---
+++ Society Management System/Models/User/HallBooking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Society_Management_System.Models
+{
+    public class HallBooking
+    {
+        public int Id { get; set; }
+        public string HallType { get; set; }
+        public DateTime BookingDate { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string Purpose { get; set; }
+    }
+}
