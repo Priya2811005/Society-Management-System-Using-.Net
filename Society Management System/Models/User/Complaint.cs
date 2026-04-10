854fc394076176e035c98e1313f837dd291c57d2
Trushali Sorathiya <sorathiyatrushali225@gmail.com>
2026-04-10 12:05:53 +0530

    User

--- Society Management System/Models/User/Complaint.cs
+++ Society Management System/Models/User/Complaint.cs
@@ -1,12 +1,20 @@
-﻿using System;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Society_Management_System.Models
 {
     public class Complaint
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Subject is required")]
+        [StringLength(200, ErrorMessage = "Subject cannot exceed 200 characters")]
         public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(1000)]
         public string Description { get; set; }
+
+        [Required]
         public DateTime CreatedAt { get; set; }
     }
 }
--- Society Management System/Models/User/Visitor.cs
+++ Society Management System/Models/User/Visitor.cs
@@ -1,11 +1,17 @@
-﻿using System;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Society_Management_System.Models
 {
     public class Visitor
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Contact number is required")]
+        [StringLength(15, MinimumLength = 10, ErrorMessage = "Valid contact number required")]
         public string Contact { get; set; }
         public DateTime VisitDate { get; set; }
         public string Purpose { get; set; }
